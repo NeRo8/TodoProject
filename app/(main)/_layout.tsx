49b6ba7fda51a18bb811747dc0dbe9cbc590010a
NeRo8 <artur.duchenko@gmail.com>
2024-12-13 16:04:39 +0200

    fix: remoe redirect to onboarding from main

--- app/(main)/_layout.tsx
+++ app/(main)/_layout.tsx
@@ -2,13 +2,11 @@ import { useEffect } from "react";
 
 import { Redirect, Tabs } from "expo-router";
 
-import { useAppContext } from "@/providers/AppProvider";
 import { useAuthContext } from "@/providers/AuthProvider";
 
 const LOGOUT_TIMER_VALUE = 1000 * 60 * 5;
 
 export default function MainLayout() {
-  const { isOnboardingComplete } = useAppContext();
   const { isAuthComplete, onChangeAuthStatus } = useAuthContext();
 
   useEffect(() => {
@@ -20,10 +18,6 @@ export default function MainLayout() {
       clearTimeout(logoutTimer);
     };
   }, [isAuthComplete, onChangeAuthStatus]);
-
-  if (!isOnboardingComplete) {
-    return <Redirect href="/onboarding" />;
-  }
 
   if (!isAuthComplete) {
     return <Redirect href="/auth" />;
